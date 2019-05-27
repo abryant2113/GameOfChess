3c5f19acbc3b5a6ce905ff09cb8710344ef01397
Austin Bryant <abryant2113@knights.ucf.edu>
2019-05-27 17:05:31 -0400

    Contains initial skeleton for game movement/logic

--- src/core/Constants.java
+++ src/core/Constants.java
@@ -3,4 +3,12 @@ package core;
 public class Constants
 {
     public final static String TITLE_STRING = "Game Of Chess - A Game of Thrones Inspired Chess Game!";
+
+    public final static int EMPTY_SPOT = -1;
+    public final static int PLAYER_ONE = 1;
+    public final static int PLAYER_TWO = 2;
+
+    public final static int PAWN_HORIZONTAL[] = new int[]{1, 0, -1};
+    public final static int PAWN_VERTICAL[] = new int[]{-1, -1, -1};
+
 }
